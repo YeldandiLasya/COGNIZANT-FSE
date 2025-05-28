241f80c4f393c3055bdff1f0d7f8f761ed8b2d5b
Yeldandi Lasya <yeldandilasya@gmail.com>
2025-05-28 13:29:56 +0530

    FactorialCalculator.java

--- Module 3/FactorialCalculator.java
+++ Module 3/FactorialCalculator.java
@@ -3,20 +3,15 @@ import java.util.Scanner;
 public class FactorialCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter a non-negative integer: ");
-        int num = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        if (num < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            long factorial = 1;
-            for (int i = 1; i <= num; i++) {
-                factorial *= i;
-            }
-            System.out.println("Factorial of " + num + " is: " + factorial);
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
         }
 
+        System.out.println("Factorial of " + number + " is " + factorial);
         scanner.close();
     }
 }
